771eadd428dc339650d64b1c97a11210fbdf0ac8
Shubham Seth <shubhamsth1234@gmail.com>
2022-01-28 09:31:01 +0530

    [add] Shubham added UC1 of EmployeePayrollService program

--- EmployeePayrollService.sql
+++ EmployeePayrollService.sql
@@ -29,3 +29,41 @@ insert into employee_payroll(EmployeeName,PhoneNumber,Address,Department,Gender,
 ('Terisa','8788616249','Jaunpur','HR','F','300000','20000','10000','20000','180000','2021-05-20','Jaunpur','INDIA');
 select * from employee_payroll;  ---Display table
 
+--CREATE PROCEDURE precedureName    -- How to create a procedure
+--AS
+--SELECT * FROM table_name   
+--GO;
+--Exec procedureName
+
+-- Creating a Store Procedure
+Create procedure SpAddEmployeeDetails
+as
+Begin
+	Select * from employee_payroll
+END
+
+exec SpAddEmployeeDetails  --Executing SP
+
+-- Alter a store Procedure by C# program
+ALTER PROCEDURE SpAddEmployeeDetails
+(
+@EmployeeName varchar(50),
+@PhoneNumber varchar(50),
+@Address varchar(255),
+@Department varchar(50),
+@Gender char(1),
+@BasicPay float,
+@Deductions float,
+@TaxablePay float,
+@Tax float,
+@NetPay float,
+@StartDate Date,
+@City varchar(50),
+@Country varchar(50)
+)
+as
+Begin
+	Insert into employee_payroll values(@EmployeeName,@PhoneNumber,@Address,@Department,@Gender,@BasicPay,@Deductions,@TaxablePay,@Tax,
+	@NetPay,@StartDate,@City,@Country)
+End
+
