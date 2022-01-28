cfe10614137be46d9231ce8331c43e2e9fc0cf5c
Shubham Seth <shubhamsth1234@gmail.com>
2022-01-28 09:35:25 +0530

    [add] Shubham added UC2 of EmployeePayrollService program

--- EmployeePayrollServiceADO.NET/EmployeeRepository.cs
+++ EmployeePayrollServiceADO.NET/EmployeeRepository.cs
@@ -37,7 +37,117 @@ namespace EmployeePayrollServiceADO.NET
             }
             return true;
         }
-       
+
+        /* UC2:- Ability for Employee Payroll Service to retrieve the Employee Payroll from the Database.
+                 - Using ODBC read the employee payroll data from the database.
+                 - Add Start Data to EmployeePayroll Class and ensure backward compatibility.
+                 - Populate the EmployeePayroll Object.
+                 - Return the list of Employee Payroll Data.
+        */
+        public void GetAllEmployeeData()
+        {
+
+            EmployeeModel employeemodel = new EmployeeModel(); //Creating Employee model class object
+            try
+            {
+                using (connection)
+                {
+
+                    string query = "select * from dbo.employee_payroll"; // Query to get all the data from table./*TableName:-dbo.payroll_service*/
+
+                    this.connection.Open(); //open connection
+
+                    SqlCommand command = new SqlCommand(query, connection); //accept query and connection
+
+                    SqlDataReader reader = command.ExecuteReader(); // Execute sqlDataReader to fetching all records
+
+                    if (reader.HasRows)     // Checking datareader has rows or not.               
+                    {
+                        while (reader.Read()) //using while loop for read multiple rows.
+                        {
+                            employeemodel.EmployeeID = reader.GetInt32(0);
+                            employeemodel.EmployeeName = reader.GetString(1);
+                            employeemodel.PhoneNumber = reader.GetString(2);
+                            employeemodel.Address = reader.GetString(3);
+                            employeemodel.Department = reader.GetString(4);
+                            employeemodel.Gender = reader.GetString(5);
+                            employeemodel.BasicPay = reader.GetDouble(6);
+                            employeemodel.Deductions = reader.GetDouble(7);
+                            employeemodel.TaxablePay = reader.GetDouble(8);
+                            employeemodel.Tax = reader.GetDouble(9);
+                            employeemodel.NetPay = reader.GetDouble(10);
+                            employeemodel.StartDate = reader.GetDateTime(11);
+                            employeemodel.City = reader.GetString(12);
+                            employeemodel.Country = reader.GetString(13);
+                            Console.WriteLine("{0},{1},{2},{3},{4},{5},{6},{7},{8},{9},{10},{11},{12},{13}", employeemodel.EmployeeID, 
+                                employeemodel.EmployeeName, employeemodel.PhoneNumber, employeemodel.Address, employeemodel.Department, 
+                                employeemodel.Gender, employeemodel.BasicPay, employeemodel.Deductions, employeemodel.TaxablePay, 
+                                employeemodel.Tax, employeemodel.NetPay, employeemodel.StartDate, employeemodel.City, employeemodel.Country);
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine(" Record Not found on Table ");
+                    }
+                    reader.Close();
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                this.connection.Close(); //Always ensuring the closing of the connection
+            }
+
+        }
+
+        public bool AddEmployee(EmployeeModel model) //insert record to the table
+        {
+
+            try
+            {
+                using (this.connection)
+                {
+                    SqlCommand command = new SqlCommand("dbo.SpAddEmployeeDetails", this.connection);   //Creating a stored Procedure for adding employees into database
+
+                    command.CommandType = CommandType.StoredProcedure; //Command type is a class to set as stored procedure
+
+                    // Adding values from employeeModel to stored procedure                     
+                    command.Parameters.AddWithValue("@EmployeeName", model.EmployeeName);
+                    command.Parameters.AddWithValue("@PhoneNumber", model.PhoneNumber);
+                    command.Parameters.AddWithValue("@Address", model.Address);
+                    command.Parameters.AddWithValue("@Department", model.Department);
+                    command.Parameters.AddWithValue("@Gender", model.Gender);
+                    command.Parameters.AddWithValue("@BasicPay", model.BasicPay);
+                    command.Parameters.AddWithValue("@Deductions", model.Deductions);
+                    command.Parameters.AddWithValue("@TaxablePay", model.TaxablePay);
+                    command.Parameters.AddWithValue("@Tax", model.Tax);
+                    command.Parameters.AddWithValue("@NetPay", model.NetPay);
+                    command.Parameters.AddWithValue("@StartDate", model.StartDate);
+                    command.Parameters.AddWithValue("@City", model.City);
+                    command.Parameters.AddWithValue("@Country", model.Country);
+                    connection.Open();
+                    var result = command.ExecuteNonQuery();
+                    connection.Close();
+
+                    if (result != 0)
+                    {
+                        return true;
+                    }
+                    return false;
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+            finally
+            {
+                connection.Close();
+            }
+        }
 
     }
 }
--- EmployeePayrollServiceADO.NET/Program.cs
+++ EmployeePayrollServiceADO.NET/Program.cs
@@ -11,11 +11,35 @@ namespace EmployeePayrollServiceADO.NET
             EmployeeRepository repository = new EmployeeRepository();  //Creating a object of EmployeeRepository class.
 
             // UC1 Ensuring the database connection using the sql connection string
-            repository.DataBaseConnection();
+            //repository.DataBaseConnection(); 
 
-
+            //UC2
+            //AddRecordInput();                  // Add Record into Store Procedure table
+            repository.GetAllEmployeeData();     // View all Records            
+            Console.WriteLine();
         }
 
-        
+        public static void AddRecordInput()
+        {
+            EmployeeRepository repository = new EmployeeRepository();//Creating a object of EmployeeRepository class.
+            EmployeeModel model = new EmployeeModel();// Adding Employee To Database
+
+            model.EmployeeName = "Shubham";
+            model.PhoneNumber = "8788616249";
+            model.Address = "Ward No.01";
+            model.Department = "HR";
+            model.Gender = "M";
+            model.BasicPay = 500000;
+            model.Deductions = 10000;
+            model.TaxablePay = 18000;
+            model.Tax = 8000;
+            model.NetPay = 300000;
+            model.StartDate = DateTime.Now;
+            model.City = "Varanasi";
+            model.Country = "India";
+
+            Console.WriteLine(repository.AddEmployee(model) ? "Record Successfully Inserted On Table" : "Failed"); //Conditional (Ternary) operator
+            // repository.GetAllEmployeeData(); //call method to show table record
+        }
     }
 }
